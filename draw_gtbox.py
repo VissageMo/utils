6bf15dd0e9949586a227131ea86b9fe928107215
VissageMo <434753117@qq.com>
2020-05-22 16:00:29 +0800

    support_robbox

--- draw_gtbox.py
+++ draw_gtbox.py
@@ -4,46 +4,95 @@ import numpy as np
 import xml.etree.ElementTree as xmlET
 from PIL import Image, ImageDraw
 
-classes = ('ignored regions','pedestrian','people','bicycle','car','van','truck','tricycle','awning-tricycle','bus','motor','others')
+from txt_xml import addRotatedShape, rotatePoint
+
+# classes = ('ignored regions','pedestrian','people','bicycle','car','van','truck','tricycle','awning-tricycle','bus','motor','others')
 
 # 把下面的路径改为自己的路径即可
-file_path_img = './VisDrone2019-DET-train/images'
-file_path_xml = './VisDrone2019-DET-train/Annotations'
-save_file_path = './VisDrone2019-DET-train/output'
+file_path_img = './images'
+file_path_xml = './annotations'
+save_file_path = './output'
 
 pathDir = os.listdir(file_path_xml)
-for idx in range(len(pathDir)): 
-    
-    filename = pathDir[idx]
-    tree = xmlET.parse(os.path.join(file_path_xml, filename))
-    objs = tree.findall('object')        
-    num_objs = len(objs)
-    boxes = np.zeros((num_objs, 5), dtype=np.uint16)
 
-    for ix, obj in enumerate(objs):
-        bbox = obj.find('bndbox')
-        x1 = float(bbox.find('xmin').text) + 1 
-        y1 = float(bbox.find('ymin').text) + 1
-        x2 = float(bbox.find('xmax').text) + 1
-        y2 = float(bbox.find('ymax').text) + 1
+def draw_bbox(pathDir):
+    for idx in range(len(pathDir)): 
+        
+        filename = pathDir[idx]
+        tree = xmlET.parse(os.path.join(file_path_xml, filename))
+        objs = tree.findall('object')        
+        num_objs = len(objs)
+        boxes = np.zeros((num_objs, 5), dtype=np.uint16)
 
-        cla = obj.find('name').text
-        label = classes.index(cla)
+        for ix, obj in enumerate(objs):
+            bbox = obj.find('bndbox')
+            x1 = float(bbox.find('xmin').text) + 1 
+            y1 = float(bbox.find('ymin').text) + 1
+            x2 = float(bbox.find('xmax').text) + 1
+            y2 = float(bbox.find('ymax').text) + 1
 
-        boxes[ix, 0:4] = [x1, y1, x2, y2]
-        boxes[ix, 4] = label
+            cla = obj.find('name').text
+            label = classes.index(cla)
 
-    image_name = os.path.splitext(filename)[0]
-    img = Image.open(os.path.join(file_path_img, image_name + '.jpg'))
+            boxes[ix, 0:4] = [x1, y1, x2, y2]
+            boxes[ix, 4] = label
 
-    draw = ImageDraw.Draw(img)
-    for ix in range(len(boxes)):
-        if boxes[ix, 0] != 0:
-            xmin = int(boxes[ix, 0])
-            ymin = int(boxes[ix, 1])
-            xmax = int(boxes[ix, 2])
-            ymax = int(boxes[ix, 3])
-            draw.rectangle([xmin, ymin, xmax, ymax], outline=(255, 0, 0))
-            draw.text([xmin, ymin], classes[boxes[ix, 4]], (255, 0, 0))
+        image_name = os.path.splitext(filename)[0]
+        img = Image.open(os.path.join(file_path_img, image_name + '.jpg'))
 
-    img.save(os.path.join(save_file_path, image_name + '.png'))
+        draw = ImageDraw.Draw(img)
+        for ix in range(len(boxes)):
+            if boxes[ix, 0] != 0:
+                xmin = int(boxes[ix, 0])
+                ymin = int(boxes[ix, 1])
+                xmax = int(boxes[ix, 2])
+                ymax = int(boxes[ix, 3])
+                draw.rectangle([xmin, ymin, xmax, ymax], outline=(255, 0, 0))
+                draw.text([xmin, ymin], classes[boxes[ix, 4]], (255, 0, 0))
+
+        img.save(os.path.join(save_file_path, image_name + '.png'))
+
+
+def draw_robbox(pathDir):
+    for idx in range(len(pathDir)): 
+        
+        filename = pathDir[idx]
+        tree = xmlET.parse(os.path.join(file_path_xml, filename))
+        objs = tree.findall('object')        
+        num_objs = len(objs)
+        boxes = np.zeros((num_objs, 8), dtype=np.uint16)
+        labels = []
+
+        for ix, obj in enumerate(objs):
+            bbox = obj.find('robndbox')
+            cx = float(bbox.find('cx').text)
+            cy = float(bbox.find('cy').text)
+            width = float(bbox.find('width').text)
+            height = float(bbox.find('height').text)
+            theta = float(bbox.find('theta').text)
+            x1, y1, x2, y2, x3, y3, x4, y4 = addRotatedShape(cx, cy, width, height, theta)
+
+            label = obj.find('name').text
+
+            boxes[ix, 0:8] = [x1, y1, x2, y2, x3, y3, x4, y4]
+            labels.append(label)
+
+        image_name = os.path.splitext(filename)[0]
+        img = Image.open(os.path.join(file_path_img, image_name + '.png'))
+
+        draw = ImageDraw.Draw(img)
+        for ix in range(len(boxes)):
+            if boxes[ix, 0] != 0:
+                # import pdb
+                # pdb.set_trace()
+                p0 = tuple(boxes[ix, 0:2])
+                p1 = tuple(boxes[ix, 2:4])
+                p2 = tuple(boxes[ix, 4:6])
+                p3 = tuple(boxes[ix, 6:8])
+                draw.polygon([p0, p1, p2, p3], outline=(255, 0, 0))
+                draw.text(p0, labels[ix], (255, 0, 0))
+
+        img.save(os.path.join(save_file_path, image_name + '.png'))
+
+if __name__ == '__main__':
+    draw_robbox(pathDir)
--- txt_xml.py
+++ txt_xml.py
@@ -1,66 +1,134 @@
 import os
+import math
+
+import numpy as np
 from PIL import Image
 
 # 把下面的路径改成你自己的路径即可
-root_dir = "./data/vis/VisDrone2019-DET-test-dev/"
-annotations_dir = root_dir+"annotations/"
+root_dir = "./"
+annotations_dir = root_dir+"txt/"
 image_dir = root_dir + "images/"
 xml_dir = root_dir+"Annotations/"
+
 # 下面的类别也换成你自己数据类别，也可适用于其他的数据集转换
 class_name = ['ignored regions','pedestrian','people','bicycle','car','van','truck','tricycle','awning-tricycle','bus','motor','others']
 
 """
 VisDrone txt: 
-    [bbox_left, bbox_top, bbox_width, bbox_height, 
-     occulation(遮挡)]
+    [bbox_left, bbox_top, bbox_width, bbox_height, occulation(遮挡)]
+
+DOTA txt:
+    [x1 y1 x2 y2 x3 y3 x4 y4 label difficult]
 """
 
-for filename in os.listdir(annotations_dir):
-    fin = open(annotations_dir+filename, 'r')
-    image_name = filename.split('.')[0]
-    img = Image.open(image_dir+image_name+".jpg") # 若图像数据是“png”转换成“.png”即可
-    xml_name = xml_dir+image_name+'.xml'
-    with open(xml_name, 'w') as fout:
-        fout.write('<annotation>'+'\n')
-        
-        fout.write('\t'+'<folder>VOC2007</folder>'+'\n')
-        fout.write('\t'+'<filename>'+image_name+'.jpg'+'</filename>'+'\n')
-        
-        fout.write('\t'+'<source>'+'\n')
-        fout.write('\t\t'+'<database>'+'VisDrone2018 Database'+'</database>'+'\n')
-        fout.write('\t\t'+'<annotation>'+'VisDrone2018'+'</annotation>'+'\n')
-        fout.write('\t\t'+'<image>'+'flickr'+'</image>'+'\n')
-        fout.write('\t\t'+'<flickrid>'+'Unspecified'+'</flickrid>'+'\n')
-        fout.write('\t'+'</source>'+'\n')
-        
-        fout.write('\t'+'<owner>'+'\n')
-        fout.write('\t\t'+'<flickrid>'+'Wenhao Mo'+'</flickrid>'+'\n')
-        fout.write('\t\t'+'<name>'+'Wenhao Mo'+'</name>'+'\n')
-        fout.write('\t'+'</owner>'+'\n')
-        
-        fout.write('\t'+'<size>'+'\n')
-        fout.write('\t\t'+'<width>'+str(img.size[0])+'</width>'+'\n')
-        fout.write('\t\t'+'<height>'+str(img.size[1])+'</height>'+'\n')
-        fout.write('\t\t'+'<depth>'+'3'+'</depth>'+'\n')
-        fout.write('\t'+'</size>'+'\n')
-        
-        fout.write('\t'+'<segmented>'+'0'+'</segmented>'+'\n')
 
-        for line in fin.readlines():
-            line = line.split(',')
-            fout.write('\t'+'<object>'+'\n')
-            fout.write('\t\t'+'<name>'+class_name[int(line[5])]+'</name>'+'\n')
-            fout.write('\t\t'+'<pose>'+'Unspecified'+'</pose>'+'\n')
-            fout.write('\t\t'+'<truncated>'+line[6]+'</truncated>'+'\n')
-            fout.write('\t\t'+'<difficult>'+str(int(line[7]))+'</difficult>'+'\n')
-            fout.write('\t\t'+'<bndbox>'+'\n')
-            fout.write('\t\t\t'+'<xmin>'+line[0]+'</xmin>'+'\n')
-            fout.write('\t\t\t'+'<ymin>'+line[1]+'</ymin>'+'\n')
-            # pay attention to this point!(0-based)
-            fout.write('\t\t\t'+'<xmax>'+str(int(line[0])+int(line[2])-1)+'</xmax>'+'\n')
-            fout.write('\t\t\t'+'<ymax>'+str(int(line[1])+int(line[3])-1)+'</ymax>'+'\n')
-            fout.write('\t\t'+'</bndbox>'+'\n')
-            fout.write('\t'+'</object>'+'\n')
-             
-        fin.close()
-        fout.write('</annotation>')
+def to_rotate_shape(bbox):
+    """calculate bbox angle
+
+    Parameters
+    ----------
+    bbox : list
+        [description]
+    """
+    bbox_int = [int(x.split('.')[0]) for x in bbox]
+    x1, y1, x2, y2, x3, y3, x4, y4 = bbox_int
+    # import pdb
+    # pdb.set_trace()
+    cx = np.average([x1, x2, x3, x4])
+    cy = np.average([y1, y2, y3, y4])
+    width = np.sqrt((x1-x2)**2 + (y1-y2)**2)
+    height = np.sqrt((x3-x2)**2 + (y3-y2)**2)
+    if x2 == x3:
+        theta = 0
+    else:
+        theta = np.arctan((y3-y2)/(x3-x2)) + np.pi/2
+    return [cx, cy, width, height, theta]
+    
+
+def rotatePoint(xc, yc, xp, yp, theta):
+    xoff = xp-xc;
+    yoff = yp-yc;
+    cosTheta = math.cos(theta)
+    sinTheta = math.sin(theta)
+    pResx = cosTheta * xoff + sinTheta * yoff
+    pResy = - sinTheta * xoff + cosTheta * yoff
+    # pRes = (xc + pResx, yc + pResy)
+    return xc+pResx, yc+pResy
+
+
+def addRotatedShape(cx, cy, w, h, angle):
+    p0x,p0y = rotatePoint(cx,cy, cx - w/2, cy - h/2, -angle)
+    p1x,p1y = rotatePoint(cx,cy, cx + w/2, cy - h/2, -angle)
+    p2x,p2y = rotatePoint(cx,cy, cx + w/2, cy + h/2, -angle)
+    p3x,p3y = rotatePoint(cx,cy, cx - w/2, cy + h/2, -angle)
+
+    points = [p0x, p0y, p1x, p1y, p2x, p2y, p3x, p3y]
+    return points
+
+
+def main():
+    for image in os.listdir(image_dir):
+        img = Image.open(image_dir+image) # 若图像数据是“png”转换成“.png”即可    
+        image_id = image.split('.')[0]
+        txt_name =  image_id+ '-v1.5hhb.txt'
+        fin = open(annotations_dir+txt_name, 'r')
+        xml_name = xml_dir+image_id+'.xml'
+
+        with open(xml_name, 'w') as fout:
+            fout.write('<annotation>'+'\n')
+            
+            fout.write('\t'+'<folder>VOC2007</folder>'+'\n')
+            fout.write('\t'+'<filename>'+image+'</filename>'+'\n')
+            
+            fout.write('\t'+'<source>'+'\n')
+            fout.write('\t\t'+'<database>'+'VisDrone2018 Database'+'</database>'+'\n')
+            fout.write('\t\t'+'<annotation>'+'VisDrone2018'+'</annotation>'+'\n')
+            fout.write('\t\t'+'<image>'+'flickr'+'</image>'+'\n')
+            fout.write('\t\t'+'<flickrid>'+'Unspecified'+'</flickrid>'+'\n')
+            fout.write('\t'+'</source>'+'\n')
+            
+            fout.write('\t'+'<owner>'+'\n')
+            fout.write('\t\t'+'<flickrid>'+'Wenhao Mo'+'</flickrid>'+'\n')
+            fout.write('\t\t'+'<name>'+'Wenhao Mo'+'</name>'+'\n')
+            fout.write('\t'+'</owner>'+'\n')
+            
+            fout.write('\t'+'<size>'+'\n')
+            fout.write('\t\t'+'<width>'+str(img.size[0])+'</width>'+'\n')
+            fout.write('\t\t'+'<height>'+str(img.size[1])+'</height>'+'\n')
+            fout.write('\t\t'+'<depth>'+'3'+'</depth>'+'\n')
+            fout.write('\t'+'</size>'+'\n')
+            
+            fout.write('\t'+'<segmented>'+'0'+'</segmented>'+'\n')
+
+            for line in fin.readlines():
+                line = line.split(' ')
+                if len(line) != 10:
+                    continue
+                bbox, name, difficult = line[:8], line[8], line[9]
+                cx, cy, width, height, theta = to_rotate_shape(bbox)
+                fout.write('\t'+'<object>'+'\n')
+                fout.write('\t\t'+'<name>'+name+'</name>'+'\n')
+                # fout.write('\t\t'+'<difficult>'+difficult+'</difficult>'+'\n')
+                fout.write('\t\t'+'<robndbox>'+'\n')
+                fout.write('\t\t\t'+'<x1>'+line[0]+'</x1>'+'\n')
+                fout.write('\t\t\t'+'<y1>'+line[1]+'</y1>'+'\n')
+                fout.write('\t\t\t'+'<x2>'+line[2]+'</x2>'+'\n')
+                fout.write('\t\t\t'+'<y2>'+line[3]+'</y2>'+'\n')
+                fout.write('\t\t\t'+'<x3>'+line[4]+'</x3>'+'\n')
+                fout.write('\t\t\t'+'<y3>'+line[5]+'</y3>'+'\n')
+                fout.write('\t\t\t'+'<x4>'+line[6]+'</x4>'+'\n')
+                fout.write('\t\t\t'+'<y4>'+line[7]+'</y4>'+'\n')
+                fout.write('\t\t\t'+'<cx>'+str(cx)+'</cx>'+'\n')
+                fout.write('\t\t\t'+'<cy>'+str(cy)+'</cy>'+'\n')
+                fout.write('\t\t\t'+'<width>'+str(width)+'</width>'+'\n')
+                fout.write('\t\t\t'+'<height>'+str(height)+'</height>'+'\n')
+                fout.write('\t\t\t'+'<theta>'+str(theta)+'</theta>'+'\n')
+
+                fout.write('\t\t'+'</robndbox>'+'\n')
+                fout.write('\t'+'</object>'+'\n')
+                
+            fin.close()
+            fout.write('</annotation>')
+
+if __name__ == '__main__':
+    main()
